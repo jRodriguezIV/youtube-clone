db28f69630f6d34bd38b8d4a01873ffbf2e8a270
jRodriguezIV <j.a.rodrigueziv@gmail.com>
2023-03-12 14:27:26 -0400

    added search bar with a button

--- src/Components/Common/Header/Header.js
+++ src/Components/Common/Header/Header.js
@@ -6,7 +6,12 @@ export default function Header () {
 
     return (
         <div className='header-wrapper'>
-            <h1>Header</h1>
+            <h1>YouTube</h1>
+
+            <label className='search-bar'  htmlFor="search">
+                <input name='search' placeholder="Search" type="text" />
+                <button>Search{/* Search Icon*/}</button>
+            </label>
         </div>
     )
 }
