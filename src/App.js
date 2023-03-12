6b774c2b23d9a514093ec4993679a04930f4d68a
jRodriguezIV <j.a.rodrigueziv@gmail.com>
2023-03-12 15:19:40 -0400

    pulled canges

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
 import Footer from './Components/Common/Footer/Footer';
 import Header from './Components/Common/Header/Header';
