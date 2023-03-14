35f7936b18b3102664c49748948bcf583f00d931
jRodriguezIV <j.a.rodrigueziv@gmail.com>
2023-03-13 20:17:01 -0400

    added searchState functionality

--- src/api/fetch.js
+++ src/api/fetch.js
@@ -3,17 +3,16 @@
 import YouTube from "react-youtube";
 
 const URL = "https://www.googleapis.com/youtube/v3/search";
-const API_KEY = "?key=AIzaSyCb93nTzM1HA9bMi8-MIvMCoRXSz5ZDWMg";
-const SEARCH_TERM = '&q=bats' //Change bats for Search State value.
+const API_KEY = "?key=AIzaSyDreUDK_wC1euAK2AT2SA9-iQpgGz2wV0k";
 const PART = "&part=snippet";
-const MAX_RESULTS = "&max_results=8";
+const MAX_RESULTS = "&max_results=12";
 
 // SEARCH_TERM = `q={searchTerm}`
 // https://www.googleapis.com/youtube/v3/search?key=AIzaSyCb93nTzM1HA9bMi8-MIvMCoRXSz5ZDWMg&q=bats&part=snippet&max_results=10
 // HTTPS+API_KEY+SEARCH_TERM
 
-export function searchByTerm() {
-  return fetch('https://www.googleapis.com/youtube/v3/search?key=AIzaSyDreUDK_wC1euAK2AT2SA9-iQpgGz2wV0k&q=bats&part=snippet&max_results=12').then((response) => {
+export function searchByTerm(searchState) {
+  return fetch(`${URL}${API_KEY}&q=${searchState}${PART}${MAX_RESULTS}`).then((response) => {
     return response.json();
   });
 }
