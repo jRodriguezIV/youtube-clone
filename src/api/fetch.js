0d433de98002efc07d6f0b18295d586acec88b25
jRodriguezIV <j.a.rodrigueziv@gmail.com>
2023-03-20 02:19:18 -0400

    updated fetch method and header for cors issue

--- src/api/fetch.js
+++ src/api/fetch.js
@@ -3,12 +3,13 @@ const API_KEY = process.env.REACT_APP_API_KEY;
 const PART = "&part=snippet";
 const MAX_RESULTS = "&max_results=12";
 
-// SEARCH_TERM = `q={searchTerm}`
-// https://www.googleapis.com/youtube/v3/search?key=AIzaSyCb93nTzM1HA9bMi8-MIvMCoRXSz5ZDWMg&q=bats&part=snippet&max_results=10
-// HTTPS+API_KEY+SEARCH_TERM
-
 export function searchByTerm(searchState) {
-  return fetch(`${URL}${API_KEY}&q=${searchState}${PART}${MAX_RESULTS}`).then((response) => response.json());
+  const options = {
+    method: "GET",
+    header: { "Allow-Control-Allow-Origin": "*" },
+  };
+  return fetch(
+    `${URL}${API_KEY}&q=${searchState}${PART}${MAX_RESULTS}`,
+    options
+  ).then((response) => response.json());
 }
-
-//GET https://youtube.googleapis.com/youtube/v3/search?key=[YOUR_API_KEY] HTTP/1.1
